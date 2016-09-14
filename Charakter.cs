d797cfad09bfe61d602a04b670a6ea5907a98913
Grunzmatte <max-linsenmeier@gmx.de>
2016-09-14 16:24:34 +0200

    1.0.3
    Char finished /Max + Dome

--- Charakter.cs
+++ Charakter.cs
@@ -9,7 +9,38 @@ namespace Projekt1
     /// <summary>
     /// Charakter wird hier eingestellt
     /// </summary>
-    class Charakter
+
+
+        // Max
+    static class Charakter
     {
+        private static string name; // Spieler Name
+        private static int health = 25; // Spieler Hp
+        private static int maxhealth = 25; // Maximale Spieler Hp
+        private static int ammountpotion = 1;
+        private static int maxammountpotion = 3;
+        private static bool nameSet = false;
+
+        internal static string Name
+        {
+            get { if (name != null) return name; else return ""; }
+            set { if (!nameSet) { name = value; nameSet = true; } }
+        }
+
+        internal static int Health
+        {
+            get { return health; }
+            set { if (health + value > maxhealth)  health = maxhealth;  else  health += value;  }
+        }
+
+        internal static int Potion
+        {
+            get { return ammountpotion; }
+            set { if (ammountpotion + value > maxammountpotion)  ammountpotion = maxammountpotion;  else  ammountpotion  += value;  }
+        }
+
+       
+
+
     }
 }
