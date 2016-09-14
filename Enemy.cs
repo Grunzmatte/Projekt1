1fe99bd375b6a6fe36ffeb26b38082e7b3e8cc90
Nuget94 <Dominique-Martin@web.de>
2016-09-14 18:57:51 +0200

    1.0.4
    Enemy + Weapon Klassen Fertig /Dome

--- Enemy.cs
+++ Enemy.cs
@@ -12,5 +12,55 @@ namespace Projekt1
 
     class Enemy
     {
+        private static Random RNGenerator = new Random();   //Zufalls Zahlen generator 
+
+        //Namenskombinationen
+        private static string[] enemyName = { "Goblin", "Ork", "Oger", "Harpie", "Schlange", "Ratte", "Polizist", "Bademeister", "Oma" };
+        private static string[] enemyNameModifikator = { "Alte(r)", "Große(r)", "Grantige(r)", "Junge(r)", "Keifende(r)" };
+
+        //Gegner Werte
+        private int health = 10;        // Gegner Hp
+        private int maxhealth = 10;     // Maximale Gegner Hp
+        private int minhealth = 0;      // Minimale Lebenspunkte
+        private string name;            //Gegner Name
+        private Weapon weapon;          //Waffe des Gegners
+
+        //Konstruktor
+        Enemy()
+        {
+            maxhealth = RNGenerator.Next(1, 10);    //Maximalleben wird Zufällig gewählt
+            health = maxhealth;                     //Leben entspricht zu beginn immer dem Max Leben
+            name = enemyNameModifikator[RNGenerator.Next(0, enemyNameModifikator.Length)] + " " + enemyName[RNGenerator.Next(0, enemyName.Length)];     //Name wird zufällig generiert
+            weapon = new Weapon();                  //Waffe wird Zufällig generiert
+        }
+
+        internal string Name
+        {
+            get                                                                     //Name darf nur abgefragt werden
+            {
+                if (name != null) return name;
+                else return "";
+            }                  
+        }
+
+        internal int Health
+        {
+            get { return health; }                                                  //Lebenspunkte abfragen
+            set                                                                     //Lebenspunkte setzen
+            {
+                if (health + value > maxhealth) health = maxhealth;                 //wenn heilung das Max Leben übersteigen würde
+                else if (health + value < minhealth) health = minhealth;            //wenn schaden das Min Leben unterschreiten würde
+                else  health += value;                                              //sonst normal lebenspunkte setzen
+            }
+        }
+        
+        internal Weapon Weapon
+        {
+            get
+            {
+                return weapon;
+            }
+
+        }
     }
 }
---
+++ Weapon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt1
+{
+    class Weapon
+    {
+        static private Random RNGenerator = new Random();       //Zufalls Zahlen Generator
+
+        //Namenskombinationen
+        static private string[] Weapons = { "Axt", "Schwert", "Stein", "Stock", "erloschene Fackel" };
+        static private string[] WeaponNameModifyer = { "alte(r)", "kleine(r)", "große(r)","stumpfe(r)" };
+        
+        //Waffen Daten
+        private string type;
+        private string name;        //Name der Waffe
+        private int damage;         //Schaden der Waffe
+        static private int minWeaponDamage = 1;     //Mindestschaden den eine Waffe machen muss
+        static private int maxWeaponDamage = 5;     //Maximalschaden den eine Waffe machen kann
+
+        internal Weapon()
+        {
+            type = Weapons[RNGenerator.Next(0, Weapons.Length)];    //Waffentyp bestimmen
+            damage = RNGenerator.Next(minWeaponDamage, maxWeaponDamage + 1);            //Waffenschaden Zufällig bestimmen
+            name = WeaponNameModifyer[RNGenerator.Next(0, WeaponNameModifyer.Length)] + " " + type;     //Waffennamen Zufällig bestimmen
+        }
+
+        public int Damage { get { return damage; }  }   //Waffenschaden abfragen
+
+        public string Name { get { return name; } }     //Waffennamen abfragen
+        
+    }
+
+    
+}
