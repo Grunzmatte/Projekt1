b836bb5171f4be4fcad99ba175d804f479f656c0
Grunzmatte <max-linsenmeier@gmx.de>
2016-09-14 18:10:55 +0200

    1.0.4
    Char comments added / Max

--- Charakter.cs
+++ Charakter.cs
@@ -17,23 +17,23 @@ namespace Projekt1
         private static string name; // Spieler Name
         private static int health = 25; // Spieler Hp
         private static int maxhealth = 25; // Maximale Spieler Hp
-        private static int ammountpotion = 1;
-        private static int maxammountpotion = 3;
-        private static bool nameSet = false;
+        private static int ammountpotion = 1; // Anzahl an Tränken 
+        private static int maxammountpotion = 3; // Maximale Anuahl an Tränken
+        private static bool nameSet = false; // Wurde der Name schon gesetzt
 
-        internal static string Name
+        internal static string Name // Methode zum Namen Setzten
         {
-            get { if (name != null) return name; else return ""; }
-            set { if (!nameSet) { name = value; nameSet = true; } }
+            get { if (name != null) return name; else return ""; } 
+            set { if (!nameSet) { name = value; nameSet = true; } } 
         }
 
-        internal static int Health
+        internal static int Health // Methode um die Health zu verändern
         {
             get { return health; }
             set { if (health + value > maxhealth)  health = maxhealth;  else  health += value;  }
         }
 
-        internal static int Potion
+        internal static int Potion // Methode zum Anzahl der Tränke ändern
         {
             get { return ammountpotion; }
             set { if (ammountpotion + value > maxammountpotion)  ammountpotion = maxammountpotion;  else  ammountpotion  += value;  }
